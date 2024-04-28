e4052f3a3adee109c5b3fc1693e0f5047ceda5fb
kingpoi06 <putratyo90@gmail.com>
2024-04-29 03:54:18 +0700

    ubah bagian buat table databasenya

--- controllers/RefreshToken.js
+++ controllers/RefreshToken.js
@@ -1,30 +1,36 @@
-import Users from "../models/UserModel.js"
-import jwt from "jsonwebtoken"
+import Users from "../models/UserModel.js";
+import jwt from "jsonwebtoken";
 
-export const refreshToken = async(req, res) =>{
+export const refreshToken = async (req, res) => {
     try {
         const refreshToken = req.cookies.refreshToken;
-        if(!refreshToken) return res.sendStatus(401);
-        const user = await Users.findAll({
+        if (!refreshToken) return res.sendStatus(401);
+
+        const user = await Users.findOne({
             where: {
                 jwt_token: refreshToken
             }
         });
-        if(!user[0]) return res.sendStatus(403);
+
+        if (!user) return res.sendStatus(403);
+
         jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
-            if(err) return res.sendStatus(403);
-            const userId = user[0].id;
-            const uuid = user[0].uuid;
-            const username = user[0].username;
-            const email = user[0].email;
-            const role = user[0].role;
-            const accessToken = jwt.sign({userId, uuid, username, email, role}, process.env.ACCESS_TOKEN_SECRET, {
+            if (err) return res.sendStatus(403);
+
+            const userId = user.id;
+            const uuid = user.uuid;
+            const username = user.username;
+            const email = user.email;
+            const role = user.role;
+
+            const accessToken = jwt.sign({ userId, uuid, username, email, role }, process.env.ACCESS_TOKEN_SECRET, {
                 expiresIn: '15s'
             });
-            res.json({ accessToken })
-            
-        })
-    } catch (error){
+
+            res.json({ accessToken });
+        });
+    } catch (error) {
         console.log(error);
+        res.sendStatus(500);
     }
-}
+};
