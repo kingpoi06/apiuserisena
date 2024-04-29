7944c9e75190ffc75b8d831d2aa523b1e579791d
kingpoi06 <putratyo90@gmail.com>
2024-04-29 18:51:06 +0700

    update refreshtoken and js-cookie

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.use(session({
 // }
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:3000', 
+    origin: 'https://feuserisena.vercel.app', 
     // origin: 'https://fe-userisena.vercel.app',
 }))
 
